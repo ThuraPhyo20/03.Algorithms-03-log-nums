2b4e027101fd5f6a3a34645c9f6dc71201c001e3
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-05-06 19:07:17 +0630

    solved

---
+++ log-nums.js
@@ -0,0 +1,11 @@
+// Write code to print all numbers from 1 to `num`
+// Assume `num` will be a positive number
+
+var logNums = function(num) {
+    
+    for(var b=1; b<=num;b=b+1){
+        console.log(b);
+    }
+
+};
+logNums(5);
